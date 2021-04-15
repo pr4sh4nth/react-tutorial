1b39f807e1cd9ba6b1bb2708c447235328db5b9c
prashanth <prash488@gmail.com>
2021-04-15 11:16:05 +0800

    created a simple table with simple and class components that use state to delete information

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Table from './Table'
+class App extends Component {
+    state = {
+        characters: [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+              },
+              {
+                name: 'Mac',
+                job: 'Bouncer',
+              },
+              {
+                name: 'Dee',
+                job: 'Aspring actress',
+              },
+              {
+                name: 'Dennis',
+                job: 'Bartender',
+              },
+        ]
+    }
+    removeCharacter = (index) => {
+        const {characters} = this.state
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        this.setState({
+            characters: characters.filter((character, i) =>{
+                return i !== index
+            })
+        })
+    }
+    render() {
+      return (
+        <div className="container">
+          <Table characterData={this.state.characters} removeCharacter={this.removeCharacter}/>
+        </div>
+      )
+    }
+  }
 
-export default App;
+  export default App
