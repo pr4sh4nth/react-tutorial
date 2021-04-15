4541075eb4081f18eb5e67df3ba96b7f029aeffc
prashanth <prash488@gmail.com>
2021-04-15 11:48:17 +0800

    Add in Create, Add functionality to the form to update state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, {Component} from 'react'
 import Table from './Table'
+import Form from './Form'
 class App extends Component {
     state = {
         characters: [
-            {
-                name: 'Charlie',
-                job: 'Janitor',
-              },
-              {
-                name: 'Mac',
-                job: 'Bouncer',
-              },
-              {
-                name: 'Dee',
-                job: 'Aspring actress',
-              },
-              {
-                name: 'Dennis',
-                job: 'Bartender',
-              },
+          
         ]
     }
     removeCharacter = (index) => {
@@ -30,10 +16,15 @@ class App extends Component {
             })
         })
     }
+
+    handleSubmit = (character) => {
+        this.setState({characters: [...this.state.characters, character]})
+    }
     render() {
       return (
         <div className="container">
           <Table characterData={this.state.characters} removeCharacter={this.removeCharacter}/>
+          <Form handleSubmit={this.handleSubmit} />
         </div>
       )
     }
